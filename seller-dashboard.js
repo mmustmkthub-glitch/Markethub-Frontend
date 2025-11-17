1c7c8844bfc360858fc5da28e541fdfa43bd3c52
mmustmkthub-glitch <mmustmkthub@gmail.com>
2025-11-17 14:50:06 +0300

    Update seller-dashboard.js

--- seller-dashboard.js
+++ seller-dashboard.js
@@ -4,7 +4,7 @@
    ✅ Integrated with Django REST API + Paystack Payments
 ========================================================= */
 
-const BASE_API = "https://mmustmkt-hub.onrender.com/api";
+/*const BASE_API = "https://mmustmkt-hub.onrender.com/api";
 const API_PRODUCTS = `${BASE_API}/products/`;
 const API_ORDERS_SELLER = `${BASE_API}/orders/mine/`;
 const API_ADS = `${BASE_API}/ads/`;
@@ -356,3 +356,4 @@ document.addEventListener("DOMContentLoaded", () => {
   loadSellerProfile();
   initSubscriptionTab();
 });
+*/
